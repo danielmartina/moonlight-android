d0c775dbe7bf6f4cbf6ae11a874afe1f35c0cfff
danielmartina <57731531+danielmartina@users.noreply.github.com>
2024-08-31 01:24:44 +0200

    Add touch mode toggle to the back game menu

--- app/src/main/java/com/limelight/ui/GameGestures.java
+++ app/src/main/java/com/limelight/ui/GameGestures.java
@@ -5,5 +5,9 @@ import com.limelight.binding.input.GameInputDevice;
 public interface GameGestures {
     void toggleKeyboard();
 
+    void toggleTouch();
+
+    boolean getTouchConfigState();
+
     void showGameMenu(GameInputDevice device);
 }
